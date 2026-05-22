0b27dc6215cccd9b173aa8cafe5aacad41dced45
Panithanxd <101002286+Panithanxd@users.noreply.github.com>
2026-05-22 08:19:48 +0700

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/DailyOS/",
   server: {
     port: 3000,
     open: true,
   },
   build: {
-    outDir: "dist",
+    outDir: "docs",
     assetsDir: "assets",
-    sourcemap: true,
+    sourcemap: false,
   },
 });
